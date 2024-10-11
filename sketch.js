1e2a53f1c4e342ee8b28d09eb43f69f2fc4576ef
luciazerboni <lucia.zerboni@mail.polimi.it>
2024-10-11 19:43:37 +0200

    ye

--- sketch.js
+++ sketch.js
@@ -13,13 +13,19 @@ function draw() {
   squareSize=30
   padding=30
 
-  let columns=(windowWidth-2*padding)/(squareSize+gutter);
-  let rows=(windowHeight-2*padding)/(squareSize+gutter);
+  let columns= floor((windowWidth-2*padding)/(squareSize+gutter));
+  let rows= floor((windowHeight-2*padding)/(squareSize+gutter));
+
+  // Ricalo del padding in modo che il disegno sia centrato
+  let usedSpace = columns*(squareSize + gutter)
+  let remainingSpace = windowWidth-usedSpace;
+  let paddingLeft = remainingSpace/2;
+
 
   for(let i=0; i<columns; i++){
    for(let r=0; r<rows; r++){
 
-    let xPos=padding + i*(squareSize+gutter) //+ random(-2.5, 2.5)
+    let xPos=paddingLeft + i*(squareSize+gutter) //+ random(-2.5, 2.5)
     let yPos=padding + r*(squareSize+gutter) //+ random(-2.5, 2.5)
     
     push(); // salva lo stato di trasformazione corrente
