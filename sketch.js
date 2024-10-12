14fd561dfd7ced65c4ccf649dc37bb78b5fe6b56
luciazerboni <lucia.zerboni@mail.polimi.it>
2024-10-12 17:03:44 +0200

    update padding

--- sketch.js
+++ sketch.js
@@ -1,50 +1,52 @@
-function setup() {
+function setup() { 
   noLoop();
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
 
-  //rettangoli piccoli
+  // Rettangoli piccoli
   fill("black");
   noStroke();
 
-  gutter=12
-  squareSize=30
-  padding=30
+  gutter = 12;
+  squareSize = 30;
+  padding = 30;
 
-  let columns= floor((windowWidth-2*padding)/(squareSize+gutter));
-  let rows= floor((windowHeight-2*padding)/(squareSize+gutter));
+  let columns = floor((windowWidth - 2 * padding) / (squareSize + gutter));
+  let rows = floor((windowHeight - 2 * padding) / (squareSize + gutter));
 
-  // Ricalcolo del padding in modo che il disegno sia centrato
-  let usedSpace = columns*(squareSize + gutter)
-  let remainingSpace = windowWidth-usedSpace;
-  let paddingLeft = remainingSpace/2;
+  // Ricalcolo del padding in modo che il disegno sia centrato orizzontalmente
+  let usedSpaceX = columns * (squareSize + gutter);
+  let remainingSpaceX = windowWidth - usedSpaceX;
+  let paddingLeft = remainingSpaceX / 2;
 
+  // Ricalcolo del padding in modo che il disegno sia centrato verticalmente
+  let usedSpaceY = rows * (squareSize + gutter);
+  let remainingSpaceY = windowHeight - usedSpaceY;
+  let paddingTop = remainingSpaceY / 2;
 
-  for(let i=0; i<columns; i++){
-   for(let r=0; r<rows; r++){
+  for (let i = 0; i < columns; i++) {
+    for (let r = 0; r < rows; r++) {
 
-    let xPos=paddingLeft + i*(squareSize+gutter) //+ random(-2.5, 2.5)
-    let yPos=padding + r*(squareSize+gutter) //+ random(-2.5, 2.5)
-    
-    push(); // salva lo stato di trasformazione corrente
+      let xPos = paddingLeft + i * (squareSize + gutter); // + random(-2.5, 2.5)
+      let yPos = paddingTop + r * (squareSize + gutter); // + random(-2.5, 2.5)
+      
+      push(); // salva lo stato di trasformazione corrente
 
-    // Trasla al centro del quadrato
-    translate(xPos + squareSize/2, yPos + squareSize/2);
+      // Trasla al centro del quadrato
+      translate(xPos + squareSize / 2, yPos + squareSize / 2);
 
-    // Condizione per ruotare random il quadrato di 45 gradi
-    if (random() < 0.5) { // 50% di probabilità di rotazione
-      rotate(PI / 4); // 45 gradi
+      // Condizione per ruotare random il quadrato di 45 gradi
+      if (random() < 0.5) { // 50% di probabilità di rotazione
+        rotate(PI / 4); // 45 gradi
+      }
+
+      // Disegna il rettangolo ruotato o non ruotato
+      rectMode(CENTER); // disegna il quadrato dal centro
+      rect(0, 0, squareSize, squareSize);
+      
+      pop(); // ripristina lo stato di trasformazione
     }
-
-    // Disegna il rettangolo ruotato o non ruotato
-    rectMode(CENTER); // disegna il quadrato dal centro
-    rect(0, 0, squareSize, squareSize);
-    
-    pop(); // ripristina lo stato di trasformazione
- 
-   }
-
   }
 }
